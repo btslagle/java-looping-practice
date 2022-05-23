36723fffa9b33ee0d70d91af603074a5021e32f6
Brad Slagle <btslagle@gmail.com>
2022-05-23 13:38:58 -0500

    Add solutions

--- App.java
+++ App.java
@@ -6,9 +6,22 @@ class App {
   }
 
   private static void forLoop() {
+    for (int i = 1; i < 11; i=i+1)
+    System.out.println(i);
   }
   private static void whileLoop() {
+    int i = 0;
+    while(i<5){      
+      System.out.println("I love Java");
+      i++;
+    }
   }
   private static void doWhileLoop() {
+    int i = 0;
+    do{
+      System.out.println("I love Java");
+      i++;
+    }
+    while(i <5);
   }
 }
